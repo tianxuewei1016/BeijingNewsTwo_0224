a1dde5eead12bc66faac67cb19dafd44a3f604f4
tianxuewei1016 <93226539@qq.com>
2017-06-06 19:17:41 +0800

    使用SwipeRefreshLayout下拉刷新

--- app/src/main/java/com/atguigu/beijingnewstwo_0224/detailpager/PhotosMenuDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnewstwo_0224/detailpager/PhotosMenuDetailPager.java
@@ -1,6 +1,7 @@
 package com.atguigu.beijingnewstwo_0224.detailpager;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -36,6 +37,8 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
     RecyclerView recyclerview;
     @InjectView(R.id.progressbar)
     ProgressBar progressbar;
+    @InjectView(R.id.refresh_layout)
+    SwipeRefreshLayout refreshLayout;
     private String url;
     private List<PhotosMenuDetailPagerBean.DataEntity.NewsEntity> datas;
     private PhotosMenuDetailPagerAdapater adapter;
@@ -50,6 +53,15 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
         //实例视图
         View view = View.inflate(context, R.layout.pager_photos_menu_detail, null);
         ButterKnife.inject(this, view);
+        //设置下拉刷新的监听
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getDataFromNet(url);
+            }
+        });
+        //设置滑动多少距离有效果
+//        refreshLayout.setDistanceToTriggerSync(100);
         return view;
     }
 
@@ -101,6 +113,8 @@ public class PhotosMenuDetailPager extends MenuDetailBasePager {
             //没有数据
             progressbar.setVisibility(View.VISIBLE);
         }
+        //隐藏刷新进度的效果
+        refreshLayout.setRefreshing(false);
     }
 
     /**
